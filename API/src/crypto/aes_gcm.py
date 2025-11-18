a4aef88db8312918ab44f75f08d013e849452e22
Dimonis97 <40121592+Dimonis97@users.noreply.github.com>
2025-11-18 12:12:17 +0200

    Implement AES-GCM encryption and decryption
    Added AES-GCM encryption and decryption functions.

--- API/src/crypto/aes_gcm.py
+++ API/src/crypto/aes_gcm.py
@@ -1,1 +1,14 @@
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
+
+
+def encrypt_aes_gcm(key: bytes, plaintext: bytes, aad: bytes = b""):
+    aes = AESGCM(key)
+    nonce = AESGCM.generate_key(bit_length=96)[:12]  # 12 байт
+    ciphertext = aes.encrypt(nonce, plaintext, aad)
+    return nonce, ciphertext
+
+
+def decrypt_aes_gcm(key: bytes, nonce: bytes, ciphertext: bytes, aad: bytes = b""):
+    aes = AESGCM(key)
+    return aes.decrypt(nonce, ciphertext, aad)
 
